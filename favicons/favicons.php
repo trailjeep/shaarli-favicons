578a246ee1300935ffea1b77c3166ac93a857551
Immánuel! <21174107+immanuelfodor@users.noreply.github.com>
2020-09-05 17:34:31 +0200

    Ensure resources loading from the right path

--- favicons/favicons.php
+++ favicons/favicons.php
@@ -9,6 +9,17 @@
 use Shaarli\Plugin\PluginManager;
 
 /**
+ * Get plugin assets base path
+ * 
+ * @param array $data - hook data
+ * 
+ * @return string - the basepath of the assets
+ */
+function favicons_get_basepath($data) {
+    return ($data['_BASE_PATH_'] ?? '') . '/' . PluginManager::$PLUGINS_PATH;
+}
+
+/**
  * Hook render_includes.
  * Executed on every page redering.
  *
@@ -27,7 +38,7 @@ function hook_favicons_render_includes($data)
 {
     // List of plugin's CSS files.
     // Note that you just need to specify CSS path.
-    $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/favicons/favicons.css';
+    $data['css_files'][] = favicons_get_basepath($data) . '/favicons/favicons.css';
     return $data;
 }
 
@@ -52,8 +63,8 @@ function hook_favicons_render_footer($data)
 {
     // List of plugin's JS files.
     // Note that you just need to specify CSS path.
-	$data['js_files'][] = PluginManager::$PLUGINS_PATH . '/favicons/jquery-3.3.1.slim.min.js';
-	$data['js_files'][] = PluginManager::$PLUGINS_PATH . '/favicons/favicons.js';
+	$data['js_files'][] = favicons_get_basepath($data) . '/favicons/jquery-3.3.1.slim.min.js';
+	$data['js_files'][] = favicons_get_basepath($data) . '/favicons/favicons.js';
     return $data;
 }
 
